f7d828aac078b9fcc4ddf3a15de60380aef9e172
luuk <luuk@ecs.csus.edu>
2021-02-08 20:50:55 -0800

    message

--- src/pages/CPUScheduling.js
+++ src/pages/CPUScheduling.js
@@ -653,8 +653,8 @@ export default function CpuScheduling(props) {
     return (
 
         <Header>
-            <h2>CPU Scheduling</h2>
-
+            <h1>CPU Scheduling</h1>
+            
             <ThemeProvider theme={theme}>
                 <Grid container direction="column">
                     <Grid item></Grid>
