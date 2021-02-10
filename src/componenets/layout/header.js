45e7e846c83aa8469c86a86dd86774f3cd8658e8
davidLgarcia <davidlouisgarcia@csus.edu>
2021-02-09 21:03:36 -0800

    added minh's sidebar

--- src/componenets/layout/header.js
+++ src/componenets/layout/header.js
@@ -12,6 +12,16 @@ import NatureIcon from '@material-ui/icons/Nature';
 import TrendingUpIcon from '@material-ui/icons/TrendingUp';
 import LinkRoute from 'react-router-dom/Link';
 import { Route, Switch } from 'react-router-dom';
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+import ListSubheader from '@material-ui/core/ListSubheader';
+import ExpandLess from '@material-ui/icons/ExpandLess';
+import ExpandMore from '@material-ui/icons/ExpandMore';
+import StarBorder from '@material-ui/icons/StarBorder';
+
 
 const drawerWidth = 250;
 
@@ -135,42 +145,81 @@ export default function PersistentDrawerLeft(props) {
                 </Grid>
         </Toolbar>
       </AppBar>
-      <Drawer
-        className={classes.drawer}
-        variant="persistent"
-        anchor="left"
-        open={open}
-        classes={{
-          paper: classes.drawerPaper,
-        }}
-      >
-        <div className={classes.drawerHeader}>
-          <IconButton onClick={handleDrawerClose}>
-            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
-          </IconButton>
-        </div>
-        <Divider />
-        
-              <p>CSC 130</p>
-        <List>
-          {CSC130.map((obj, index) => (
-            <Link href={obj.url} ><ListItem button key={obj.name}>
-              <ListItemIcon> <obj.logo></obj.logo></ListItemIcon>
-              <ListItemText primary={obj.name} />
-            </ListItem></Link>
-          ))}
-                  </List>  
-        <Divider />
-        <p>CSC 139</p>
-        <List>
-          {CSC139.map((obj, index) => (
-           <Link href={obj.url}> <ListItem button key={obj.name}>
-              <ListItemIcon><obj.logo/></ListItemIcon>
-              <ListItemText primary={obj.name} />
-            </ListItem> </Link>
-          ))}
-        </List>
-      </Drawer>
+          <Drawer
+              className={classes.drawer}
+              variant="persistent"
+              anchor="left"
+              open={open}
+              classes={{
+                  paper: classes.drawerPaper,
+              }}
+          >
+              <div className={classes.drawerHeader}>
+                  <IconButton onClick={handleDrawerClose}>
+                      {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+                  </IconButton>
+              </div>
+              <Divider />
+
+
+              <Accordion>
+                  <AccordionSummary
+                      expandIcon={<ExpandMoreIcon />}
+                      aria-controls="panel1a-content"
+                      id="panel1a-header"
+
+                  >
+                      <Typography className={classes.heading}>CSC 130</Typography>
+                  </AccordionSummary>
+                  <AccordionDetails>
+                      <Typography>
+                          <Collapse in={open} timeout="auto" unmountOnExit>
+                              <List component="div" disablePadding>
+
+                                  {CSC130.map((obj, index) => (
+                                      <Link href={obj.url} ><ListItem button key={obj.name}>
+                                          <ListItemIcon> <obj.logo></obj.logo></ListItemIcon>
+                                          <ListItemText primary={obj.name} />
+                                      </ListItem></Link>
+                                  ))}
+                              </List>
+                          </Collapse>
+                      </Typography>
+                  </AccordionDetails>
+              </Accordion>
+              <Divider />
+
+
+
+              <Divider />
+              <Accordion>
+                  <AccordionSummary
+                      expandIcon={<ExpandMoreIcon />}
+                      aria-controls="panel2a-content"
+                      id="panel2a-header"
+
+                  >
+                      <Typography className={classes.heading}>CSC 139</Typography>
+                  </AccordionSummary>
+                  <AccordionDetails>
+                      <Typography>
+                          <Collapse in={open} timeout="auto" unmountOnExit>
+                              <List component="div" disablePadding>
+                                  {CSC139.map((obj, index) => (
+                                      <Link href={obj.url}> <ListItem button key={obj.name}>
+                                          <ListItemIcon><obj.logo /></ListItemIcon>
+                                          <ListItemText primary={obj.name} />
+                                      </ListItem> </Link>
+                                  ))}
+                              </List>
+                          </Collapse>
+                      </Typography>
+                  </AccordionDetails>
+              </Accordion>
+              <Divider />
+
+          </Drawer>
+
       <main
         className={clsx(classes.content, {
           [classes.contentShift]: open,
