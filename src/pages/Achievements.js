6b900da24d1a06d8fdcac72a61a43763a0030494
SpencerBroden <sbroden@csus.edu>
2021-02-17 11:26:56 -0800

    add search feature to Achievements.js by Spencer

--- src/pages/Achievements.js
+++ src/pages/Achievements.js
@@ -1,41 +1,48 @@
-import React, { useState } from 'react'
-import Header from "../componenets/layout/header"
-import { Button, Grid, Paper, Divider, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField, MenuItem, InputLabel, FormControl, Select } from "@material-ui/core"
+import React, { useState } from 'react';
+import Header from "../componenets/layout/header";
+import { Button, Grid, Paper, Divider, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, MenuItem, InputLabel, FormControl, Select } from "@material-ui/core"
 import { makeStyles, ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-import { grey, orange } from '@material-ui/core/colors';
+import { grey } from '@material-ui/core/colors';
 import MenuList from '@material-ui/core/MenuList';
-import PropTypes from 'prop-types';
 import Modal from '@material-ui/core/Modal';
 import Backdrop from '@material-ui/core/Backdrop';
-import { useSpring, animated } from 'react-spring/web.cjs';
 import Typography from '@material-ui/core/Typography';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-
+import SearchBar from "material-ui-search-bar";
 
 const useStyles = makeStyles((theme) => ({
     root: {
         flexGrow: 1,
+        backgroundColor: "transparent",
     },
     paper: {
         padding: theme.spacing(2),
         textAlign: 'center',
         color: theme.palette.text.secondary,
+        backgroundColor: "transparent",
+        boxShadow: "none",
+        overflow: "hidden",
         height: "100%",
         width: "100%"
     },
     paperOverlay: {
         backgroundColor: theme.palette.background.paper,
-        border: '2px solid #000',
         boxShadow: theme.shadows[5],
         padding: theme.spacing(2, 4, 3),
+        outline: 0,
+        borderRadius: 1,
     },
     divider: {
         width: "50%"
     },
     body: {
         width: "40%"
+    },
+    formControl: {
+      margin: theme.spacing(1),
+      minWidth: 180,
     },
     table: {
         minWidth: 700,
@@ -70,55 +77,25 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
-
-
 function createData(name, status, date, details) {
     return { name, status, date, details };
 }
 
-const rows = [
-    createData('Achievement1', 'Completed', 'Completed on X/X/XXXX', 'view'),
-    createData('Achievement2', 'Incomplete'),
-    createData('Achievement3', 'Incomplete'),
-    createData('Achievement4', 'Incomplete'),
-    createData('Achievement5', 'Incomplete'),
+const originalRows = [
+    createData('Binary Search Traversal', 'Completed', 'Completed on X/X/XXXX', 'view'),
+    createData('Graphing', 'Incomplete'),
+    createData('Sorting', 'Incomplete'),
+    createData('CPU Scheduling', 'Incomplete'),
+    createData('Page Replacement', 'Incomplete'),
+    createData('Disk Scheduling', 'Incomplete'),
 ];
-
-const Fade = React.forwardRef(function Fade(props, ref) {
-    const { in: open, children, onEnter, onExited, ...other } = props;
-    const style = useSpring({
-        from: { opacity: 0 },
-        to: { opacity: open ? 1 : 0 },
-        onStart: () => {
-            if (open && onEnter) {
-                onEnter();
-            }
-        },
-        onRest: () => {
-            if (!open && onExited) {
-                onExited();
-            }
-        },
-    });
-
-    return (
-        <animated.div ref={ref} style={style} {...other}>
-            {children}
-        </animated.div>
-    );
-});
-
-Fade.propTypes = {
-    children: PropTypes.element,
-    in: PropTypes.bool.isRequired,
-    onEnter: PropTypes.func,
-    onExited: PropTypes.func,
-};
 
 export default function Achievements() {
     const classes = useStyles();
-    const [sort, setSort] = React.useState('');
-    const [open, setOpen] = React.useState(false);
+    const [sort, setSort] = useState('');
+    const [open, setOpen] = useState(false);
+    const [rows, setRows] = useState(originalRows);
+    const [searched, setSearched] = useState("");
 
     const handleOpen = () => {
         setOpen(true);
@@ -132,13 +109,25 @@ export default function Achievements() {
         setSort(event.target.value);
     };
 
+    const requestSearch = (searchedVal) => {
+        const filterRows = originalRows.filter((row) => {
+          return row.name.toLowerCase().includes(searchedVal.toLowerCase());
+        });
+        setRows(filterRows);
+      };
+    
+    const cancelSearch = () => {
+        setSearched("");
+        requestSearch(searched);
+    };
+
     const theme = createMuiTheme({
         palette: {
             primary: {
                 main: grey[900],
             }
         }
-    })
+    });
 
     return (
         <Header>
@@ -146,19 +135,19 @@ export default function Achievements() {
                 <Paper className={classes.paper}>
                     <Grid container alignItems={"center"} direction={"row"} justify={"center"} spacing={"5"}>
                         <Grid item><h1>Achievements</h1></Grid>
-                        <Grid item><TextField id="filled-basic" label="Search" variant="filled" /></Grid>
+                        <Grid item><SearchBar value={searched} 
+                                              onChange={(searchVal) => requestSearch(searchVal)} 
+                                              onCancelSearch={() => cancelSearch()} 
+                        /></Grid>
                         <Grid item>
                             <FormControl variant="outlined" className={classes.formControl}>
-                                <InputLabel id="simple-select">Sort</InputLabel>
+                                <InputLabel>Sort by</InputLabel>
                                 <Select
                                     labelId="simple-select"
-                                    id="simple-select"
                                     value={sort}
                                     onChange={handleChange}
-                                    label="Sort"
+                                    label="Sort by"
                                 >
-                                    <MenuItem value="">
-                                    </MenuItem>
                                     <MenuItem value={1}>Achievement</MenuItem>
                                     <MenuItem value={2}>Status</MenuItem>
                                     <MenuItem value={3}>Completition Date</MenuItem>
@@ -194,7 +183,6 @@ export default function Achievements() {
                                                 <TableCell className={classes.tc} align="center">{
                                                     <div>
                                                         <buttons type="button" className={classes.button} onClick={handleOpen}>
-
                                                             <MenuList>
                                                                 <MenuItem align="center">View</MenuItem>
                                                             </MenuList>
@@ -208,25 +196,31 @@ export default function Achievements() {
                                                             closeAfterTransition
                                                             BackdropComponent={Backdrop}
                                                             BackdropProps={{
-                                                                timeout: 500,
+                                                              classes: {
+                                                              root: classes.root
+                                                              }
+                                                            }}
+                                                            PaperProps ={{
+                                                              classes: {
+                                                              root: classes.paper
+                                                              }
                                                             }}
                                                         >
-                                                            <Fade in={open}>
                                                                 <div className={classes.paperOverlay}>
                                                                     <Card className={classes.root}>
                                                                         <CardContent>
                                                                             <Typography className={classes.title} color="textSecondary" gutterBottom>
                                                                                 Sample Icon
-                                                    </Typography>
+                                                                            </Typography>
                                                                             <Typography variant="h5" component="h2">
                                                                                 Name: Sample Achievement
-                                                    </Typography>
+                                                                            </Typography>
                                                                             <Typography className={classes.pos} color="textSecondary">
                                                                                 Description: A sample description of the achievement.
-                                                    </Typography>
+                                                                            </Typography>
                                                                             <Typography variant="body2" component="p">
                                                                                 Date: Completed on X/X/XXXX
-                                                      <br />
+                                                                            <br />
                                                                                 {'Category: A Sample Category for the achievement'}
                                                                             </Typography>
                                                                         </CardContent>
@@ -235,7 +229,6 @@ export default function Achievements() {
                                                                         </CardActions>
                                                                     </Card>
                                                                 </div>
-                                                            </Fade>
                                                         </Modal>
                                                     </div>
                                                 }</TableCell>
@@ -247,7 +240,6 @@ export default function Achievements() {
                             </TableContainer>
                         </Grid>
                     </Grid>
-
                 </Paper>
             </ThemeProvider>
         </Header>
