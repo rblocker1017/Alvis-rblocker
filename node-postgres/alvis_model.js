cca631137cd8b601f19ff48c4bfadd8c0f8f4356
Ryan Blocker <rblocker@student.sierracollege.edu>
2021-03-05 13:29:46 -0800

    very unsafe connection for testing

--- node-postgres/alvis_model.js
+++ node-postgres/alvis_model.js
@@ -1,15 +1,23 @@
-const Pool = require('pg').Pool
+const {Pool} = require('pg')
 
-const pool = new Pool({
+/*const pool = new Pool({
   user: 'alvis',
   host: 'localhost',
   database: 'alvis_db',
   password: 'buzzdev123',
   port: 5432,
+});*/
+
+let connString = 'postgres://iwqzigmkdqwrsx:d56e6f5bdac388d778c65d81cca2ad6d169263bab208239266e728bc53cea03b@ec2-54-90-55-211.compute-1.amazonaws.com:5432/dd3df33hrh6sfk';
+
+const pool = new Pool({
+  connectionString : connString,
+  ssl: {rejectUnauthorized: false}
 });
 
+
 const getUsers = (request, response) => {
-    pool.query('SELECT * FROM login ORDER BY id ASC', (error, results) => {
+    pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
       if (error) {
         throw error
       }
