3859cee0261583c0f2a969a07ca8d96610740741
davidLgarcia <davidlouisgarcia@csus.edu>
2021-03-11 23:57:24 -0800

    fixed tree bug

--- src/pages/GraphingAlgorithm.js
+++ src/pages/GraphingAlgorithm.js
@@ -93,7 +93,7 @@ function pointValues(circle, type) {
     if (circle.start) {
         return "START NODE"
     }
-    else if (circle.end && (type !== "Prim")) {
+    else if (circle.end && type !== "Prim" && type !== "Kruskal") {
         return "END NODE"
     }
     return "";
