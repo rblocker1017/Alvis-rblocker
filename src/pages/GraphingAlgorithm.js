d7899d80d653659ef6fc8a7aac9990e7f727cf89
davidLgarcia <davidlouisgarcia@csus.edu>
2021-03-11 19:08:29 -0800

    fixed bottom button spacing

--- src/pages/GraphingAlgorithm.js
+++ src/pages/GraphingAlgorithm.js
@@ -666,21 +666,6 @@ export default function GraphingAlgorithm() {
                                                 <Button variant="contained" color="primary" onClick={stepForward}>Step Forward</Button>
                                             </Grid>
                                             <Grid item xs={2}>
-                                                <InputLabel >Connector Value</InputLabel>
-                                                <Select
-                                                    value={conValue}
-                                                    onChange={changeConValue}
-                                                >
-                                                    <MenuItem value={1}>1</MenuItem>
-                                                    <MenuItem value={2}>2</MenuItem>
-                                                    <MenuItem value={3}>3</MenuItem>
-                                                    <MenuItem value={4}>4</MenuItem>
-                                                    <MenuItem value={5}>5</MenuItem>
-                                                    <MenuItem value={6}>6</MenuItem>
-                                                    <MenuItem value={7}>7</MenuItem>
-                                                    <MenuItem value={8}>8</MenuItem>
-                                                    <MenuItem value={9}>9</MenuItem>
-                                                </Select>
                                             </Grid>
                                             <Grid item>
                                                 <Button variant="contained" color="primary" onClick={setStart}>Set Start</Button>
