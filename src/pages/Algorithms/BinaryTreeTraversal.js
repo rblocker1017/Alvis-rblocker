ffe08f20b57ac67ca6f24a719e8f67e1743020f8
xDeja <61637301+xDeja@users.noreply.github.com>
2021-03-29 11:37:29 -0700

    Properly working test for BTT

--- src/pages/Algorithms/BinaryTreeTraversal.js
+++ src/pages/Algorithms/BinaryTreeTraversal.js
@@ -1,14 +1,14 @@
 export function inOrderTraversal(root, array, circles){
     if (root !== undefined) {
         if (root.leftChild !== null)
-            inOrderTraversal(circles.find((circle) => circle.id === root.leftChild.id), array, circles);
+            inOrderTraversal(circles.find((circle) => circle.id === root.leftChild), array, circles);
         circles.forEach((circle) => {
             if (circle.id === root.id) {
                 array.push(circle);
             }
         });
         if (root.rightChild !== null)
-            inOrderTraversal(circles.find((circle) => circle.id === root.rightChild.id), array, circles);
+            inOrderTraversal(circles.find((circle) => circle.id === root.rightChild), array, circles);
     }
 };
 
@@ -18,18 +18,19 @@ export function preOrderTraversal(root, array, circles){
             if (circle.id === root.id) array.push(circle);
         });
         if (root.leftChild !== null)
-            preOrderTraversal(circles.find((circle) => circle.id === root.leftChild.id), array, circles);
+            preOrderTraversal(circles.find((circle) => circle.id === root.leftChild), array, circles);
         if (root.rightChild !== null)
-            preOrderTraversal(circles.find((circle) => circle.id === root.rightChild.id), array, circles);
+            preOrderTraversal(circles.find((circle) => circle.id === root.rightChild), array, circles);
     }
+    console.log(array);
 };
 
 export function postOrderTraversal(root, array, circles){
     if (root !== undefined) {
         if (root.leftChild !== null)
-            postOrderTraversal(circles.find((circle) => circle.id === root.leftChild.id), array, circles);
+            postOrderTraversal(circles.find((circle) => circle.id === root.leftChild), array, circles);
         if (root.rightChild !== null)
-            postOrderTraversal(circles.find((circle) => circle.id === root.rightChild.id), array, circles);
+            postOrderTraversal(circles.find((circle) => circle.id === root.rightChild), array, circles);
         circles.map((circle) => {
             if (circle.id === root.id) array.push(circle);
         });
--- src/pages/Test/BinaryTreeTraversal.test.js
+++ src/pages/Test/BinaryTreeTraversal.test.js
@@ -5,25 +5,23 @@ test("inOrderTraversal", () => {
   circles.push({
     id: circles.length,
     parent: null,
+    leftChild: 1,
+    rightChild: 2,
+  });
+  circles.push({
+    id: circles.length,
+    parent: 0,
     leftChild: null,
     rightChild: null,
   });
   circles.push({
     id: circles.length,
-    parent: circles[0],
+    parent: 0,
     leftChild: null,
     rightChild: null,
   });
-  circles.push({
-    id: circles.length,
-    parent: circles[0],
-    leftChild: null,
-    rightChild: null,
-  });
-  circles[0].leftChild = circles[1];
-  circles[0].rightChild = circles[2];
   let inputCircles = [];
   inOrderTraversal(circles[0], inputCircles, circles)
-  let circlesOutput = [circles[1], circles[0], circles[2]];
-  expect(inputCircles).toEqual(circlesOutput);
+  let circlesOutput = [circles.find((circle) => circle.id === circles[0].leftChild), circles[0], circles.find((circle) => circle.id === circles[0].rightChild)];
+  expect(circlesOutput).toEqual(inputCircles);
 });
