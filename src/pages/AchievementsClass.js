eeced48f5c6b07ca5e2a72ff120ee77a6894efa2
7648504 <evanelias1623@mgail.com>
2021-03-31 21:41:32 -0700

    trying to get unique details cards for each achievement

--- src/pages/AchievementsClass.js
+++ src/pages/AchievementsClass.js
@@ -1,5 +1,30 @@
-export default class Achievement {
+import React, { useState } from 'react';
+import Typography from '@material-ui/core/Typography';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import { makeStyles, ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import { Button, Grid, Paper, Divider, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, MenuItem, InputLabel, FormControl, Select } from "@material-ui/core"
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+        backgroundColor: "transparent",
+    },
+    paper: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+        backgroundColor: "transparent",
+        boxShadow: "none",
+        overflow: "hidden",
+        height: "100%",
+        width: "100%"
+    }
+}));
+
+export default class Achievement {
+    
     constructor(name){
         this.achievementName = name;
         this.completed = false;
@@ -40,6 +65,34 @@ export default class Achievement {
         }
         
     }
+
+     
+
+    /*get card(){
+        return  <div className={classes.paperOverlay}>
+            <Card className={classes.root}>
+                <CardContent>
+                    <Typography className={classes.title} color="textSecondary" gutterBottom>
+                        Sample Icon
+                        </Typography>
+                    <Typography variant="h5" component="h2">
+                        Name: {this.name}
+                        </Typography>
+                    <Typography className={classes.pos} color="textSecondary">
+                        Description: {this.description}
+                        </Typography>
+                    <Typography variant="body2" component="p">
+                        Date: Completed on X/X/XXXX
+                        <br />
+                        {'Category: A Sample Category for the achievement'}
+                    </Typography>
+                </CardContent>
+                <CardActions>
+                    <Button size="small">Learn More</Button>
+                </CardActions>
+            </Card>
+        </div>
+    }*/
 
 
 }
